8a0282b08511b105848945411f61942f1bac7d31
jenmlucas <jenmlucas1@gmail.com>
2022-02-07 19:51:23 -0700

    editing server connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/deep-thoughts', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-module.exports = mongoose.connection;
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/deep-thoughts',
+  {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+  }
+);
